d28c54a8e22355e6336aec2370f67a0a45abdbad
Anik98B <anikbhowmik0188@gmail.com>
2024-08-18 01:49:17 +0600

    added vercel.json file

---
+++ README.md
@@ -0,0 +1,4 @@
+## for locally run->
+* npm i express
+* npm i cors
+* npm i dotenv
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require('express'); 
 const app = express();
-const cors=require('cors');
+const cors = require('cors');
 require('dotenv').config();
-const port =process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
@@ -14,35 +14,41 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
 
-    const productsCollection = client.db("ezoneDB").collection("products");
+        const productsCollection = client.db("ezoneDB").collection("products");
 
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        //product collection
+        app.get('/products', async (req, res) => {
+            const result = await productsCollection.find().toArray();
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('eZone prime is Running')
+    res.send('eZone prime is Running')
 })
 
 app.listen(port, () => {
-  console.log(`eZone prime is Running on port ${port}`)
+    console.log(`eZone prime is Running on port ${port}`)
 })
